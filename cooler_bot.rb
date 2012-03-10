fc8994ee2ba3f07034b19fd479f1bf8b34bec309
e115763 <e115763@gmail.com>
2012-03-10 11:54:33 +0900

    add comment

--- cooler_bot.rb
+++ cooler_bot.rb
@@ -5,10 +5,10 @@ require 'user_stream'
 require 'twitter'
 require 'pp'
 
-  consumer_key = gets.chomp
-  consumer_secret = gets.chomp
-  oauth_token = gets.chomp
-  oauth_token_secret = gets.chomp
+consumer_key = gets.chomp
+consumer_secret = gets.chomp
+oauth_token = gets.chomp
+oauth_token_secret = gets.chomp
 
 UserStream.configure do |config|
   config.consumer_key = consumer_key
@@ -27,7 +27,8 @@ end
 client = UserStream.client
 
 def check_tags tags,tag_text
-  
+  # ハッシュタグのチェック
+
   tags.each do |tag|
     return true if tag.text == tag_text
   end
@@ -35,16 +36,19 @@ def check_tags tags,tag_text
 end
 
 def reply tweet
-  
+  # tweet に対してリプライする
+
   option = {"in_reply_to_status_id"=>tweet.id}
   msg = "@#{tweet.user.screen_name} クーラーください"
-  
+
   Twitter.update msg,option
 end
 
 client.user do |status|
   if status.has_key? "text"
+    # text を持ってる時。 普通のtweet。
     if !status.entities.hashtags.empty?
+      # ハッシュタグを持ってる時
       if check_tags(status.entities.hashtags , "ギークハウス沖縄")
         reply status
       end
